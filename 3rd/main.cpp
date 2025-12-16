720e94c349d00fa8c553926b0b56c0e82dcf3668
Christopher <christopher.develop@hotmail.com>
2025-12-16 23:02:12 +0800

    refactor: Comments and spacing.

--- 3rd/main.cpp
+++ 3rd/main.cpp
@@ -17,7 +17,7 @@ int MaxDoubleDigitNumberGivenLine(const std::vector<int>& bank)
     }
 
     const auto maxNumberLocation = std::max_element(bank.begin(), bank.end());
-    if (maxNumberLocation == bank.end() - 1)
+    if (maxNumberLocation == bank.end() - 1) // If the max number is at the end
     {
         // Search back from this point for the next largest.
         const auto secondMaxNumberLocation = std::max_element(bank.begin(), maxNumberLocation);
@@ -34,12 +34,11 @@ int MaxDoubleDigitNumberGivenLine(const std::vector<int>& bank)
 
 int main()
 {
-    const std::string fileName = "input.txt";
-    std::ifstream file(fileName);
-
+    const std::string FileName = "input.txt";
+    std::ifstream file(FileName);
     if(!file.is_open())
     {
-        std::cerr << "Error: Unable to open file: " << fileName << std::endl;
+        std::cerr << "Error: Unable to open file: " << FileName << "\n";
         exit(1);
     }
 
@@ -47,9 +46,11 @@ int main()
     int total = 0;
     while(std::getline(file, line))
     {
+        // Convert the chars into ints
         std::vector<int> numbers;
         std::transform(line.begin(), line.end(), std::back_inserter(numbers), [](char c) {return c - '0';});
 
+        // Accumulate the result
         const int result = MaxDoubleDigitNumberGivenLine(numbers);
         total += result;
     }
